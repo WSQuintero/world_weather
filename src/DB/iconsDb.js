bff7f13706038567642bb64705e16aeb3d0dd6bf
Santiago Quintero <santiagoquinteroperfil@gmail.com>
2023-11-01 12:00:32 -0500

    corrigiendo rutas de imagenes

--- src/DB/iconsDb.js
+++ src/DB/iconsDb.js
@@ -1,22 +1,30 @@
 const icons = [
-  { '01d': '/img/sol.png' },
-  { '01n': '/img/sol.png' },
-  { '02d': '/img/solyNube.png' },
-  { '02n': '/img/solyNube.png' },
-  { '03d': '/img/nublado.png' },
-  { '03n': '/img/nublado.png' },
-  { '04d': '/img/pocaLluvia.png' },
-  { '04n': '/img/pocaLluvia.png' },
-  { '09d': '/img/lluvia.png' },
-  { '09n': '/img/lluvia.png' },
-  { '10d': '/img/tormenta.png' },
-  { '10n': '/img/tormenta.png' },
-  { '11d': '/img/tormentaElectrica.png' },
-  { '11n': '/img/tormentaElectrica.png' },
-  { '13d': '/img/nieve.png' },
-  { '13n': '/img/nieve.png' },
-  { '50d': '/img/nieve.png' },
-  { '50n': '/img/nieve.png' }
+  { '01d': 'https://i.ibb.co/MN3gKhF/sol.png' },
+  { '01n': 'https://i.ibb.co/MN3gKhF/sol.png' },
+  { '02d': 'https://i.ibb.co/gvbmGrC/solyNube.png' },
+  { '02n': 'https://i.ibb.co/gvbmGrC/solyNube.png' },
+  { '03d': 'https://i.ibb.co/XjMdzDS/nublado.png' },
+  { '03n': 'https://i.ibb.co/XjMdzDS/nublado.png' },
+  { '04d': 'https://i.ibb.co/2N6nvQ8/poca-Lluvia.png' },
+  { '04n': 'https://i.ibb.co/2N6nvQ8/poca-Lluvia.png' },
+  { '09d': 'https://i.ibb.co/NF717zX/lluvia.png' },
+  { '09n': 'https://i.ibb.co/NF717zX/lluvia.png' },
+  { '10d': 'https://i.ibb.co/5WPNQxC/tormenta.png' },
+  { '10n': 'https://i.ibb.co/5WPNQxC/tormenta.png' },
+  { '11d': 'https://i.ibb.co/7vcKzzv/tormenta-Electrica.png' },
+  { '11n': 'https://i.ibb.co/7vcKzzv/tormenta-Electrica.png' },
+  { '13d': 'https://i.ibb.co/GWmNK05/nieve.png' },
+  { '13n': 'https://i.ibb.co/GWmNK05/nieve.png' },
+  { '50d': 'https://i.ibb.co/GWmNK05/nieve.png' },
+  { '50n': 'https://i.ibb.co/GWmNK05/nieve.png' }
 ]
 
 export { icons }
+
+
+
+
+
+
+
+
