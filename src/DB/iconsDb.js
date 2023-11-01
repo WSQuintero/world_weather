6db29322f3041cebc203228dd785ba81af0e228a
Santiago Quintero <santiagoquinteroperfil@gmail.com>
2023-11-01 17:56:23 -0500

    Corregido tamaño deimagenes

--- src/DB/iconsDb.js
+++ src/DB/iconsDb.js
@@ -1,25 +1,32 @@
 const icons = [
-  { '01d': 'https://i.ibb.co/MN3gKhF/sol.png' },
-  { '01n': 'https://i.ibb.co/MN3gKhF/sol.png' },
-  { '02d': 'https://i.ibb.co/gvbmGrC/solyNube.png' },
-  { '02n': 'https://i.ibb.co/gvbmGrC/solyNube.png' },
-  { '03d': 'https://i.ibb.co/XjMdzDS/nublado.png' },
-  { '03n': 'https://i.ibb.co/XjMdzDS/nublado.png' },
-  { '04d': 'https://i.ibb.co/2N6nvQ8/poca-Lluvia.png' },
-  { '04n': 'https://i.ibb.co/2N6nvQ8/poca-Lluvia.png' },
-  { '09d': 'https://i.ibb.co/NF717zX/lluvia.png' },
-  { '09n': 'https://i.ibb.co/NF717zX/lluvia.png' },
-  { '10d': 'https://i.ibb.co/5WPNQxC/tormenta.png' },
-  { '10n': 'https://i.ibb.co/5WPNQxC/tormenta.png' },
-  { '11d': 'https://i.ibb.co/7vcKzzv/tormenta-Electrica.png' },
-  { '11n': 'https://i.ibb.co/7vcKzzv/tormenta-Electrica.png' },
-  { '13d': 'https://i.ibb.co/GWmNK05/nieve.png' },
-  { '13n': 'https://i.ibb.co/GWmNK05/nieve.png' },
-  { '50d': 'https://i.ibb.co/GWmNK05/nieve.png' },
-  { '50n': 'https://i.ibb.co/GWmNK05/nieve.png' }
+  { '01d': 'https://i.ibb.co/yXgJNH8/sol.png' },
+  { '01n': 'https://i.ibb.co/yXgJNH8/sol.png' },
+  { '02d': 'https://i.ibb.co/WKTMsmC/solyNube.png' },
+  { '02n': 'https://i.ibb.co/WKTMsmC/solyNube.png' },
+  { '03d': 'https://i.ibb.co/TBWWKcd/nublado.png' },
+  { '03n': 'https://i.ibb.co/TBWWKcd/nublado.png' },
+  { '04d': 'https://i.ibb.co/yPnQcqF/poca-Lluvia.png' },
+  { '04n': 'https://i.ibb.co/yPnQcqF/poca-Lluvia.png' },
+  { '09d': 'https://i.ibb.co/WvWK135/lluvia.png' },
+  { '09n': 'https://i.ibb.co/WvWK135/lluvia.png' },
+  { '10d': 'https://i.ibb.co/frxK5jD/tormenta.png' },
+  { '10n': 'https://i.ibb.co/frxK5jD/tormenta.png' },
+  { '11d': 'https://i.ibb.co/N6zxRWv/tormenta-Electrica.png' },
+  { '11n': 'https://i.ibb.co/N6zxRWv/tormenta-Electrica.png' },
+  { '13d': 'https://i.ibb.co/dD9sTgx/nieve.png' },
+  { '13n': 'https://i.ibb.co/dD9sTgx/nieve.png' },
+  { '50d': 'https://i.ibb.co/dD9sTgx/nieve.png' },
+  { '50n': 'https://i.ibb.co/dD9sTgx/nieve.png' }
 ]
 
 export { icons }
+
+
+
+
+
+
+
 
 
 
